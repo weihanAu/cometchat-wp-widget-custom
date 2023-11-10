58df1790dda9c4f2e8ff8b5790dbcab129e0e340
liaozhengyang <liaozhengyang@star-net.cn>
2023-11-10 17:02:48 +0800

    update->chatheader

--- CometChatWorkspace/src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Messages/CometChatMessageHeader/style.js
+++ CometChatWorkspace/src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Messages/CometChatMessageHeader/style.js
@@ -159,3 +159,48 @@ export const chatOptionStyle = (img, context, ongoingCall) => {
 		},
 	};
 };
+
+export const chatsHeaderMinimumStyle = (img, theme) => {
+	const mq = [...theme.breakPoints];
+
+	return {
+		cursor: "pointer",
+		display: "block",
+		background: `url(${img}) no-repeat bottom`,
+		width: "36px",
+		height: "36px",
+		[`@media ${mq[0]}`]: {
+			display: "block!important",
+		},
+	};
+};
+
+export const chatsHeaderDisposeStyle = (img, theme) => {
+	const mq = [...theme.breakPoints];
+
+	return {
+		cursor: "pointer",
+		display: "block",
+		background: `url(${img}) no-repeat center`,
+		width: "36px",
+		height: "36px",
+		[`@media ${mq[0]}`]: {
+			display: "block!important",
+		},
+	};
+};
+
+export const chatsHeaderDuplicateStyle = (img, theme) => {
+	const mq = [...theme.breakPoints];
+
+	return {
+		cursor: "pointer",
+		display: "block",
+		background: `url(${img}) no-repeat center`,
+		width: "36px",
+		height: "36px",
+		[`@media ${mq[0]}`]: {
+			display: "block!important",
+		},
+	};
+};
